10704d97ffae7b3c31a8c577534a3fca6eaff47d
ivanlewin <57846638+ivanlewin@users.noreply.github.com>
2020-09-21 22:35:53 -0300

    descargo los mp3 del kick y el snare y los seteo locales

--- app.js
+++ app.js
@@ -69,11 +69,4 @@ class MetronomeApp {
 }
 
 
-
-const metronomeApp = new MetronomeApp( '',
-    ['https://dl.dropboxusercontent.com/s/h9sow482vkw06xe/dinky-kick.mp3',"https://dl.dropboxusercontent.com/s/kkikcupdg9n1qiy/dinky-snare.mp3" ],
-    VisSettings);
-
-
-
-   
+const metronomeApp = new MetronomeApp('', ["./mp3/dinky-kick.mp3","./mp3/dinky-snare.mp3"], VisSettings);
